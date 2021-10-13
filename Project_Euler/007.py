8b39fd2ad8d48005ff7d906ed402ce83b4269306
Alexey <53459471+r-alexey@users.noreply.github.com>
2021-10-13 18:04:47 +0300

    Update 007.py

--- Project_Euler/007.py
+++ Project_Euler/007.py
@@ -2,14 +2,14 @@ def return_n_prime_number(n):
    
     prime_number_list = [2]
     prime_number_qty = 1
-    current_number = 2
+    current_number = 1
    
     while prime_number_qty < n:
-       
-        current_number += 1
+
+        current_number += 2
         is_prime = True
        
-        for prime in prime_number_list:
+        for prime in prime_number_list[1:]:
             if current_number % prime == 0:
                 is_prime = False
                 break
@@ -18,4 +18,4 @@ def return_n_prime_number(n):
             prime_number_list.append(current_number)
             prime_number_qty += 1
    
-    return current_number
+    return prime_number_list[-1]
