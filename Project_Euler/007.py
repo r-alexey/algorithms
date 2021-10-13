692ef734d41cbf2dab0d2675adc0bb19640d4259
Alexey <53459471+r-alexey@users.noreply.github.com>
2021-10-13 17:40:43 +0300

    Update 007.py

--- Project_Euler/007.py
+++ Project_Euler/007.py
@@ -1,20 +1,21 @@
-array = [i for i in range(150000)]
-array [1] = 0
-qty = 0
-
-
-for number in array:
-    if number == 0:
-        continue
-    else:
-        qty += 1
-        if qty == 10001:
-            break
-        for zero_number in array[number * 2 :: number]:
-            array[zero_number] = 0
-
-
-if qty == 10001:
-    print (number)
-else:
-    print('10 001st prime number out of range')
+def return_n_prime_number(n):
+   
+    prime_number_list = [2]
+    prime_number_qty = 1
+    current_number = 2
+   
+    while prime_number_qty < n:
+       
+        current_number += 1
+        is_prime = True
+       
+        for prime in prime_number_list:
+            if current_number % prime == 0:
+                is_prime = False
+                break
+               
+        if is_prime:
+            prime_number_list.append(current_number)
+            prime_number_qty += 1
+   
+    return current_number
